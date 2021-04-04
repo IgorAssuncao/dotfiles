caf1430f3638647db756914eb38c1bc7dd397d28
Igor Assuncao <igor.ormonde@gmail.com>
2021-04-04 00:31:47 -0300

    Updated start.sj

--- start.sh
+++ start.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 # sh -c "$(curl -H 'Cache-Control: no-cache' -LsSo- https://raw.githubusercontent.com/IgorAssuncao/system-config/master/start.sh)"
 
-echo "This script automatically installs some tools and creates symlinks for you."
-echo "All of these tools will be prompted to install:
+showMessage() {
+  local message=$1
+  echo $message
+}
+
+checkInput() {
+  read -p "Please, answer (y/n): " -n 1 choice
+  if [ $choice != "y" ]; then
+    exit 0
+  fi
+}
+
+showMessage "This script automatically installs some tools and creates symlinks for you."
+showMessage "All of these tools will be prompted to install:
     - QTile (A window manager written in python)
     - Kitty (A terminal emulator)
     - Kitty custom config
@@ -12,42 +24,33 @@ echo "All of these tools will be prompted to install:
     - Custom init.vim (nvim config file)
     - Custom Xresources
   "
-echo "Do you want to continue?"
-read -p "Please, answer (y/n): " -n 1 choice && \
-  if [ $choice != "y" ]; then
-    exit 0
-  fi
 
-echo "Downloading repo" && \
-  git clone https://github.com/IgorAssuncao/system-config.git ~/system-config && \
-  echo "Finished downloading repo"
+showMessage "Downloading repo"
+git clone https://github.com/IgorAssuncao/system-config.git ~/system-config
+showMessage "Finished downloading repo"
 
-echo "Install custom i3 config?"
-read -p "Please, answer (y/n): " -n 1 choice && \
-  if [ $choice == "y" ]; then
-      if [ -d "~/.i3/config" ]; then
-        echo "Renaming ~.i3/config to ~/.i3/config.bkp" && \
-        mv ~/.i3/config ~/.i3/config.bkp
-        echo "Creating ~/.i3/config symlink" && \
-        ln -s ~/system-config/.i3/config ~/.i3/config && \
-        echo "Finished creating ~/.i3/config symlink"
-      fi
-  fi
+showMessage "Install custom i3 config?"
+checkInput
+if [ -d "~/.i3/config" ]; then
+  echo "Renaming ~.i3/config to ~/.i3/config.bkp" && \
+  mv ~/.i3/config ~/.i3/config.bkp
+  echo "Creating ~/.i3/config symlink" && \
+  ln -s ~/system-config/.i3/config ~/.i3/config && \
+  echo "Finished creating ~/.i3/config symlink"
+fi
 
-echo "Install Qtile?"
-read -p "Please, answer (y/n): " -n 1 choice && \
-  if [ $choice == "y" ]; then
-    echo "Installing Qtile" && \
-      sudo pacman -S qtile && \
-      echo "Finished installed Qtile" && \
-      echo "Creating qtile symlink" && \
-      if [ -d "~/.config/qtile" ]; then
-        echo "Renaming ~/.config/qtile to ~/.config/qtile.bkp"
-        mv ~/.config/qtile ~/.config/qtile.bkp
-      fi && \
-        ln -s ~/system-config/.config/qtile/ ~/.config/qtile && \
-        echo "Finished creating qtile symlink"
-  fi
+showMessage "Install Qtile?"
+checkInput
+echo "Installing Qtile" && \
+  sudo pacman -S qtile && \
+  echo "Finished installed Qtile" && \
+  echo "Creating qtile symlink" && \
+  if [ -d "~/.config/qtile" ]; then
+    echo "Renaming ~/.config/qtile to ~/.config/qtile.bkp"
+    mv ~/.config/qtile ~/.config/qtile.bkp
+  fi && \
+    ln -s ~/system-config/.config/qtile/ ~/.config/qtile && \
+    echo "Finished creating qtile symlink"
 
 echo "Install Kitty?"
 read -p "Please, answer (y/n): " -n 1 choice && \
