ad6db1ebc944ab121f167addd8db41518817808b
Igor Assuncao <igor.ormonde@gmail.com>
2021-04-04 00:48:44 -0300

    Changed Git commit command.

--- .config/nvim/plugins-config/fugitive.vim
+++ .config/nvim/plugins-config/fugitive.vim
@@ -1,5 +1,5 @@
 nmap <leader>gh :diffget //3<CR>
 nmap <leader>gf :diffget //2<CR>
 nmap <leader>gs :G<CR>
-nmap <leader>gc :Gcommit<CR>
+nmap <leader>gc :Git commit<CR>
 nmap <leader>gp :Gpush<CR>
--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 # sh -c "$(curl -H 'Cache-Control: no-cache' -LsSo- https://raw.githubusercontent.com/IgorAssuncao/system-config/master/start.sh)"
 
-showMessage() {
-  local message=$1
-  echo $message
-}
-
 checkInput() {
   read -p "Please, answer (y/n): " -n 1 choice
   if [ $choice != "y" ]; then
@@ -13,8 +8,8 @@ checkInput() {
   fi
 }
 
-showMessage "This script automatically installs some tools and creates symlinks for you."
-showMessage "All of these tools will be prompted to install:
+echo "This script automatically installs some tools and creates symlinks for you."
+echo "All of these tools will be prompted to install:
     - QTile (A window manager written in python)
     - Kitty (A terminal emulator)
     - Kitty custom config
@@ -25,113 +20,96 @@ showMessage "All of these tools will be prompted to install:
     - Custom Xresources
   "
 
-showMessage "Downloading repo"
-git clone https://github.com/IgorAssuncao/system-config.git ~/system-config
-showMessage "Finished downloading repo"
+echo "Downloading repo"
+git clone git@github.com:IgorAssuncao/system-config.git
+echo "Finished downloading repo"
 
-showMessage "Install custom i3 config?"
+echo "Install custom i3 config?"
 checkInput
 if [ -d "~/.i3/config" ]; then
-  echo "Renaming ~.i3/config to ~/.i3/config.bkp" && \
+  echo "Renaming ~.i3/config to ~/.i3/config.bkp"
   mv ~/.i3/config ~/.i3/config.bkp
-  echo "Creating ~/.i3/config symlink" && \
-  ln -s ~/system-config/.i3/config ~/.i3/config && \
+  echo "Creating ~/.i3/config symlink"
+  ln -s ~/system-config/.i3/config ~/.i3/config
   echo "Finished creating ~/.i3/config symlink"
 fi
 
-showMessage "Install Qtile?"
+echo "Install Qtile?"
 checkInput
-echo "Installing Qtile" && \
-  sudo pacman -S qtile && \
-  echo "Finished installed Qtile" && \
-  echo "Creating qtile symlink" && \
-  if [ -d "~/.config/qtile" ]; then
-    echo "Renaming ~/.config/qtile to ~/.config/qtile.bkp"
-    mv ~/.config/qtile ~/.config/qtile.bkp
-  fi && \
-    ln -s ~/system-config/.config/qtile/ ~/.config/qtile && \
-    echo "Finished creating qtile symlink"
+echo "Installing Qtile"
+sudo pacman -S qtile
+echo "Finished installed Qtile"
+echo "Creating qtile symlink"
+if [ -d "~/.config/qtile" ]; then
+  echo "Renaming ~/.config/qtile to ~/.config/qtile.bkp"
+  mv ~/.config/qtile ~/.config/qtile.bkp
+fi
+ln -s ~/system-config/.config/qtile/ ~/.config/qtile
+echo "Finished creating qtile symlink"
 
 echo "Install Kitty?"
-read -p "Please, answer (y/n): " -n 1 choice && \
-  if [ $choice == "y" ]; then
-    echo "Installing kitty" && \
-      sudo pacman -S kitty && \
-      echo "Finished installing kitty"
-      echo "Creating kitty symlink" && \
-      if [ -d "~/.config/kitty" ]; then
-        echo "Renaming ~/.config/kitty to ~/.config/kitty.bkp"
-        mv ~/.config/kitty ~/.config/kitty.bkp
-      fi && \
-        ln -s ~/system-config/.config/kitty/ ~/.config/kitty && \
-        echo "Finished creating kitty symlink"
-  fi
+checkInput
+echo "Installing kitty"
+sudo pacman -S kitty
+echo "Finished installing kitty"
+echo "Creating kitty symlink"
 
 echo "Install custom Kitty config?"
-read -p "Please, answer (y/n): " -n 1 choice && \
-  if [ $choice == "y" ]; then
-    echo "Creating kitty symlink" && \
-    if [ -d "~/.config/kitty" ]; then
-      echo "Renaming ~/.config/kitty to ~/.config/kitty.bkp"
-      mv ~/.config/kitty ~/.config/kitty.bkp
-    fi && \
-      ln -s ~/system-config/.config/kitty/ ~/.config/kitty && \
-      echo "Finished creating kitty symlink"
-  fi
+checkInput
+echo "Creating kitty symlink"
+if [ -d "~/.config/kitty" ]; then
+  echo "Renaming ~/.config/kitty to ~/.config/kitty.bkp"
+  mv ~/.config/kitty ~/.config/kitty.bkp
+fi
+ln -s ~/system-config/.config/kitty/ ~/.config/kitty
+echo "Finished creating kitty symlink"
+
 
 echo "Install Oh-my-zsh?"
-read -p "Please, answer (y/n): " -n 1 choice && \
-  if [ $choice == "y" ]; then
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
-      echo "Creating ~/.zshrc symlink" && \
-      if [ -a "~/.zshrc" ]; then
-        echo "Renaming ~/.zshrc to ~/.zshrc.bkp"
-        mv ~/.zshrc ~/.zshrc.bkp
-      fi && \
-        ln -s ~/system-config/.config/zsh/.zshrc ~/.zshrc && \
-        echo "Finished creating ~/.zshrc symlink"
-  fi
+checkInput
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Creating ~/.zshrc symlink"
+if [ -a "~/.zshrc" ]; then
+  echo "Renaming ~/.zshrc to ~/.zshrc.bkp"
+  mv ~/.zshrc ~/.zshrc.bkp
+fi
+ln -s ~/system-config/.config/zsh/.zshrc ~/.zshrc
+echo "Finished creating ~/.zshrc symlink"
 
 echo "Install custom vimrc?"
-read -p "Please, answer (y/n): " -n 1 choice && \
-  if [ $choice == "y" ]; then
-    if [ -a "~/.vimrc" ]; then
-      echo "Renaming ~/.vimrc to ~/.vimrc.bkp"
-      mv ~/.vimrc ~/.vimrc.bkp
-    fi
-    echo "Creating ~/.vimrc symlink" && \
-      ln -s ~/system-config/.config/vim/vimrc ~/.vimrc
-    echo "Creating ~/.vim/coc-settings synlink" && \
-      ln -s ~/system-config/.config/vim/coc-settings.json ~/.vim/coc-settings.json
-  fi
+checkInput
+if [ -a "~/.vimrc" ]; then
+  echo "Renaming ~/.vimrc to ~/.vimrc.bkp"
+  mv ~/.vimrc ~/.vimrc.bkp
+fi
+echo "Creating ~/.vimrc symlink"
+ln -s ~/system-config/.config/vim/vimrc ~/.vimrc
+echo "Creating ~/.vim/coc-settings synlink"
+ln -s ~/system-config/.config/vim/coc-settings.json ~/.vim/coc-settings.json
 
 echo "Install NeoVim?"
-read -p "Please, answer (y/n): " -n 1 choice && \
-  if [ $choice == "y" ]; then
-    echo "Installing NeoVim" && \
-      sudo pacman -S neovim && \
-      echo "Finished installing NeoVim"
-  fi
+read -p "Please, answer (y/n): " -n 1 choice
+if [ $choice == "y" ]; then
+  echo "Installing NeoVim"
+  sudo pacman -S neovim
+  echo "Finished installing NeoVim"
+fi
 
 echo "Install custom init.vim (nvim config file)?"
-read -p "Please, answer (y/n): " -n 1 choice && \
-  if [ $choice == "y" ]; then
-    if [ -d "~/.config/nvim" ]; then
-      echo "Renaming ~/.config/nvim to ~/.config/nvim.bkp"
-      mv ~/.config/nvim ~/.config/nvim.bkp
-    fi
-    echo "Creating ~/.config/nvim symlink" && \
-      ln -s ~/system-config/.config/nvim ~/.config/nvim
-  fi
+checkInput
+if [ -d "~/.config/nvim" ]; then
+  echo "Renaming ~/.config/nvim to ~/.config/nvim.bkp"
+  mv ~/.config/nvim ~/.config/nvim.bkp
+fi
+echo "Creating ~/.config/nvim symlink"
+ln -s ~/system-config/.config/nvim ~/.config/nvim
 
 echo "Install custom Xresources?"
-read -p "Please, answer (y/n): " -n 1 choice && \
-  if [ $choice == "y" ]; then
-    echo "Renaming ~/.Xresources to ~/.Xresources.bkp"
-    mv ~/.Xresources ~/.Xresources.bkp
-    echo "Renaming ~/.xinitrc to ~/.xinitrc.bkp"
-    mv ~/.xinitrc ~/.xinit.bkp
-    echo "Creating symlinks" && \
-      ln -s ~/system-config/.config/x/.Xresources ~/.Xresources && \
-      ln -s ~/system-config/.config/x/.xinitrc ~/.xinitrc
-  fi
+checkInput
+echo "Renaming ~/.Xresources to ~/.Xresources.bkp"
+mv ~/.Xresources ~/.Xresources.bkp
+echo "Renaming ~/.xinitrc to ~/.xinitrc.bkp"
+mv ~/.xinitrc ~/.xinit.bkp
+echo "Creating symlinks" && \
+ln -s ~/system-config/.config/x/.Xresources ~/.Xresource
+ln -s ~/system-config/.config/x/.xinitrc ~/.xinitrc
