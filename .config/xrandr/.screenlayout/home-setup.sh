c80a74016afa8e82a93260f88a13f1fa9320ac66
Igor Assuncao <igor.ormonde@gmail.com>
2021-06-03 17:26:15 -0300

    Remove comments in xrandrSetup

--- .config/xrandr/.screenlayout/home-setup.sh
+++ .config/xrandr/.screenlayout/home-setup.sh
@@ -35,8 +35,6 @@ setupXrandr() {
     --output DP-3 --off \
     --output HDMI-0 --mode 2560x1080 --rate 74.99 --pos 0x0 --rotate normal \
     --output DP-4 --primary --mode 1920x1080 --rate 143.98 --pos 2560x0 --rotate normal \
-    # --output HDMI-0 --mode 2560x1080 --rate 74.99 --pos 0x420 --rotate normal \
-    # --output DP-4 --primary --mode 1920x1080 --rate 143.98 --pos 2560x0 --rotate right \
     --output DP-5 --off
 }
 
