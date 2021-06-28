d8b78105ef87382864cf277a45b92a5255537544
Igor Assuncao <igor.ormonde@gmail.com>
2021-06-28 01:44:46 -0300

    Change monitors order horizontally and vertically

--- .config/xrandr/.screenlayout/home-setup.sh
+++ .config/xrandr/.screenlayout/home-setup.sh
@@ -33,10 +33,10 @@ setupXrandr() {
     --output DP-1 --off \
     --output DP-2 --off \
     --output DP-3 --off \
-    --output HDMI-0 --mode 2560x1080 --rate 74.99 --pos 1920x0 --rotate normal \
+    --output HDMI-0 --primary --mode 2560x1080 --rate 74.99 --pos 0x0 --rotate normal \
     --output DP-4 --off \
-    --output DP-5 --primary --mode 1920x1080 --rate 60 --pos 0x0 --rotate normal
-    # --output DP-5 --primary --mode 1920x1080 --rate 143.98 --pos 2560x0 --rotate normal
+    --output DP-5 --mode 1920x1080 --rate 60 --pos 2560x0 --rotate normal
+    # --output DP-5 --mode 1920x1080 --rate 143.98 --pos 2560x0 --rotate normal
 }
 
 setupVerticalMonitor() {
@@ -45,9 +45,9 @@ setupVerticalMonitor() {
     --output DP-1 --off \
     --output DP-2 --off \
     --output DP-3 --off \
-    --output HDMI-0 --mode 2560x1080 --rate 74.99 --pos 1920x420 --rotate normal \
+    --output HDMI-0 --primary --mode 2560x1080 --rate 74.99 --pos 0x420 --rotate normal \
     --output DP-4 --off \
-    --output DP-5 --primary --mode 1920x1080 --rate 60 --pos 0x0 --rotate right
+    --output DP-5 --mode 1920x1080 --rate 60 --pos 2560x0 --rotate right
     # --output DP-5 --primary --mode 1920x1080 --rate 143.98 --pos 2560x0 --rotate right
 }
 
