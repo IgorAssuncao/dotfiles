e73c5ce36f08cb9e325df0b61cf1967957be64ad
Igor Assuncao <igor.ormonde@gmail.com>
2022-07-03 18:31:36 -0300

    Organize symlinks in script.

--- scripts/create-symlinks.sh
+++ scripts/create-symlinks.sh
@@ -1,8 +1,25 @@
+### Window managers
+## i3
 ln -s ~/system-config/config/.i3 ~/.i3
-ln -s ~/system-config/config/alacritty ~/.config/alacritty
+
+### Git
 ln -s ~/system-config/config/git/.gitconfig ~/.gitconfig
 ln -s ~/system-config/config/git/.gitignore ~/.gitignore
-ln -s ~/system-config/config/nvim ~/.config/nvim
+
+
+### Shells
+## ZSH
 ln -s ~/system-config/config/zsh/.zshrc ~/.zshrc
 ln -s ~/system-config/config/zsh/.zshrc_aliases ~/.zshrc_aliases
 ln -s ~/system-config/config/zsh/.zshrc_functions ~/.zshrc_functions
+
+### Terminal Emulators
+## Alacritty 
+# ln -s ~/system-config/config/alacritty ~/.config/alacritty
+
+## Kitty
+# ln -s ~/system-config/config/kitty ~/.config/kitty
+
+### Text editors
+## NeoVim
+ln -s ~/system-config/config/nvim ~/.config/nvim
