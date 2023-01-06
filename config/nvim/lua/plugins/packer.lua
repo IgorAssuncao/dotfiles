5b11b9fa1d647da513cb5d4de915a04a2bfa7702
Igor Assuncao <igor.ormonde@gmail.com>
2023-01-06 04:01:48 -0300

    (feat) add autopairing.

--- config/nvim/lua/plugins/packer.lua
+++ config/nvim/lua/plugins/packer.lua
@@ -3,14 +3,14 @@
 
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
   -- -- TODO: Custom packer install_path.
   -- local config_dir = vim.api.nvim_list_runtime_paths()[1]
-  -- local install_path = config_dir .. '/packer.nvim'
+  -- local install_path = config_dir .. "/packer.nvim"
 
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
     vim.cmd [[packadd packer.nvim]]
     return true
   end
@@ -19,80 +19,88 @@ end
 
 local packer_bootstrap = ensure_packer()
 
--- local packer = require('packer')
+-- local packer = require("packer")
 -- TODO: Change packer git default_url_format to use SSH
 -- packer.init(
 --   {
 --     git = {
--- 	  default_url_format = 'git@github.com:%s'
+-- 	  default_url_format = "git@github.com:%s"
 -- 	}
 --   }
 -- )
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use "wbthomason/packer.nvim"
 
   use {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.0',
-    requires = { { 'nvim-lua/plenary.nvim' } }
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.0",
+    requires = { { "nvim-lua/plenary.nvim" } }
   }
 
   use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate"
   }
 
-  use 'nvim-treesitter/playground'
+  use "nvim-treesitter/playground"
 
-  -- use 'theprimeagen/harpoon'
+  -- use "theprimeagen/harpoon"
 
-  use 'mbbill/undotree'
+  use "mbbill/undotree"
 
-  use 'tpope/vim-fugitive'
+  use "tpope/vim-fugitive"
 
   use {
-    'VonHeikemen/lsp-zero.nvim',
+    "VonHeikemen/lsp-zero.nvim",
     requires = {
       -- LSP Support
-      { 'neovim/nvim-lspconfig' },
-      { 'williamboman/mason.nvim' },
-      { 'williamboman/mason-lspconfig.nvim' },
+      { "neovim/nvim-lspconfig" },
+      { "williamboman/mason.nvim" },
+      { "williamboman/mason-lspconfig.nvim" },
 
       -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
-      { 'saadparwaiz1/cmp_luasnip' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/cmp-nvim-lua' },
+      { "hrsh7th/nvim-cmp" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-nvim-lua" },
 
       -- Snippets
-      { 'L3MON4D3/LuaSnip' },
-      { 'rafamadriz/friendly-snippets' },
+      { "L3MON4D3/LuaSnip" },
+      { "rafamadriz/friendly-snippets" },
     }
   }
 
   use {
-    'lewis6991/gitsigns.nvim',
+    "windwp/nvim-autopairs",
     requires = {
-      'nvim-lua/plenary.nvim'
+      "hrsh7th/nvim-cmp"
     },
-    config = function() require('gitsigns').setup() end
+    config = function() require("nvim-autopairs").setup {} end,
   }
 
   use {
-    'EdenEast/nightfox.nvim',
-    as = 'nightfox',
+    "lewis6991/gitsigns.nvim",
+    requires = {
+      "nvim-lua/plenary.nvim"
+    },
+    config = function() require("gitsigns").setup() end
+  }
+
+  use {
+    "EdenEast/nightfox.nvim",
+    as = "nightfox",
     config = function()
-      vim.cmd('colorscheme nightfox')
+      vim.cmd("colorscheme nightox")
     end
   }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
-    require('packer').sync()
+    require("packer").sync()
   end
 end)
