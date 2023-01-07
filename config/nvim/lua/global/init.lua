5843fe17fde3c139aa0ab5e5631ce8aca4be3599
Igor Assuncao <igor.ormonde@gmail.com>
2023-01-07 01:22:58 -0300

    (feat) add highlight on yank.

--- config/nvim/lua/global/init.lua
+++ config/nvim/lua/global/init.lua
@@ -1,9 +1,10 @@
 require("global.base")
 require("global.remaps")
+require("global.text")
 
 -- local home = os.getenv("HOME") .. "/"
 -- local asdf_shims_path = home .. ".asdf/shims"
--- 
+--
 -- vim.api.nvim_command("set runtimepath^=" .. asdf_shims_path)
 -- vim.api.nvim_command('let &packpath = &runtimepath')
 -- vim.api.nvim_command('source ~/.vim/vimrc')
