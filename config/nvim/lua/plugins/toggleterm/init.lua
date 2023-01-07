5f9d837183b4c7828c7bb6794bf1da4eb47c66bd
Igor Assuncao <igor.ormonde@gmail.com>
2023-01-07 18:30:24 -0300

    (chore) remap toggleterm keys and add htop option.

--- config/nvim/lua/plugins/toggleterm/init.lua
+++ config/nvim/lua/plugins/toggleterm/init.lua
@@ -1,13 +1,13 @@
 require("toggleterm").setup({
   direction = "float",
-  open_mapping = "<leader>tn",
+  open_mapping = [[<C-\>t]],
   insert_mappings = false,
-  close_on_exit = false
+  close_on_exit = true
 })
 
 function Toggleterm_set_terminal_keymaps()
   local opts = { buffer = 0 }
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', [[<C-\><C-\>]], [[<C-\><C-n>]], opts)
   vim.keymap.set('t', '<C-h>', [[<Cmd>wicmd h<CR>]], opts)
   vim.keymap.set('t', '<C-j>', [[<Cmd>wicmd j<CR>]], opts)
   vim.keymap.set('t', '<C-k>', [[<Cmd>wicmd k<CR>]], opts)
@@ -26,18 +26,18 @@ vim.api.nvim_create_autocmd(
 
 local terminal = require("toggleterm.terminal").Terminal
 
-local cwd_terminal = terminal:new({ dir = "git_dir", direction = "float" })
+local git_terminal = terminal:new({ dir = "git_dir", direction = "float" })
 
-local function _toggle_cwd_terminal()
-  cwd_terminal:toggle()
+local function _toggle_git_terminal()
+  git_terminal:toggle()
 end
 
-vim.keymap.set("n", "<leader>tt", function() _toggle_cwd_terminal() end)
+vim.keymap.set("n", [[<C-\>g]], function() _toggle_git_terminal() end)
 
-local htop_terminal = terminal:new({ cmd = "htop", hidden = true })
+local htop_terminal = terminal:new({ cmd = "htop", hidden = true, close_on_exit = true })
 
 local function _toggle_htop_terminal()
   htop_terminal:toggle()
 end
 
-vim.keymap.set("n", "<leader>th", function() _toggle_htop_terminal() end)
+vim.keymap.set("n", [[<C-\>h]], function() _toggle_htop_terminal() end)
