3dd4e07dc87620d1a16955d46a90ab9b82bc7e8c
Igor Assuncao <igor.ormonde@gmail.com>
2023-01-18 03:34:21 -0300

    chore: improve nvim-tree.

--- config/nvim/lua/plugins/nvim-tree/init.lua
+++ config/nvim/lua/plugins/nvim-tree/init.lua
@@ -1,13 +1,55 @@
 require("nvim-tree").setup {
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true
+  },
   filters = {
     dotfiles = true
   },
+  git = {
+    enable = true,
+    ignore = false
+  },
   renderer = {
-    group_empty = false
+    group_empty = false,
+    highlight_git = true,
+    full_name = true,
+    icons = {
+      webdev_colors = true,
+      git_placement = "before",
+      modified_placement = "before",
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true
+      }
+    },
+    indent_markers = {
+      enable = true,
+      icons = {
+        bottom = "-",
+        corner = "└ ",
+        edge = "| ",
+        item = "| ",
+        none = " "
+      }
+    },
+  },
+  update_focused_file = {
+    enable = true,
+    update_cwd = false,
+    ignore_list = {}
   },
   view = {
     adaptive_size = true,
-    side = "left"
+    hide_root_folder = false,
+    number = true,
+    relativenumber = true,
+    side = "left",
+    float = {
+      enable = false
+    }
   }
 }
 
