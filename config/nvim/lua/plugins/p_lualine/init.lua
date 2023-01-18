cb5e471a5c12c9dc61ff5cf01b945d23ea8256ee
IgorAssuncao <igor.ormonde@gmail.com>
2023-01-18 12:15:25 -0300

    chore: add window number in lualine.

--- config/nvim/lua/plugins/p_lualine/init.lua
+++ config/nvim/lua/plugins/p_lualine/init.lua
@@ -1,13 +1,27 @@
+local function window()
+  return "W: " .. tostring(vim.api.nvim_win_get_number(0))
+
+end
+
 PLUGINS.lualine.setup({
   options = {
     theme = "auto" -- or horizon
   },
   sections = {
-    lualine_a = { "mode" },
+    lualine_a = { window, "mode" },
     lualine_b = { "branch" },
     lualine_c = { { "filename", path = 1 } },
+    -- lualine_x = {},
     lualine_y = { "diagnostics" },
     lualine_z = { "location" }
+  },
+  inactive_sections = {
+    lualine_a = { window },
+    -- lualine_b = { "branch" },
+    lualine_c = { { "filename", path = 1 } },
+    -- lualine_x = {},
+    -- lualine_y = { "diagnostics" },
+    -- lualine_z = { "location" }
   }
 })
 
