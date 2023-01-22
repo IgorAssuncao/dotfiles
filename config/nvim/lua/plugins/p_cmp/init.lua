0d33b188b1c6609813d883113d455163ae0d94a1
Igor Assuncao <igor.ormonde@gmail.com>
2023-01-22 13:36:33 -0300

    refact: remove old cmp file.

--- config/nvim/lua/plugins/p_cmp/init.lua
+++ config/nvim/lua/plugins/p_cmp/init.lua
@@ -18,12 +18,42 @@ local lspkind = require("lspkind")
 
 local cmp_mappings = require("plugins.p_cmp.keymaps").cmp_mappings
 
+-- PLUGINS.cmp.setup({
+--   formatting = {
+--     format = lspkind.cmp_format({
+--       mode = "symbol_text",
+--       menu = (menu)
+--     })
+--   },
+--   mapping = cmp_mappings
+-- })
+
 PLUGINS.cmp.setup({
   formatting = {
-    format = lspkind.cmp_format({
-      mode = "symbol_text",
-      menu = (menu)
-    })
+    fields = { "abbr", "kind", "menu" },
+    format = function(entry, vim_item)
+      local f = lspkind.cmp_format({ mode = "symbol_text" })(entry, vim_item)
+      local strings = vim.split(f.kind, "%s", { trimempty = true })
+      f.kind = " " .. (strings[1] or "") .. " "
+      f.menu = "(" .. (strings[2] or "") .. ")"
+
+      return f
+    end
   },
-  mapping = cmp_mappings
+  mapping = cmp_mappings,
+  -- sources = PLUGINS.cmp.config.sources(
+  --   {
+  --     { name = "LuaSnip" },
+  --     {
+  --       name = "nvim_lua",
+  --       entry_filter = function(entry, ctx)
+  --         print(entry)
+  --         return true
+  --       end
+  --     }
+  --   },
+  --   {
+  --     { name = "friendly-snippets" }
+  --   }
+  -- )
 })
