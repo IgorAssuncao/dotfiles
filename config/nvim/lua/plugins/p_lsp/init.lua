23d519464160a733de1894a8db69bee092db157c
Igor Assuncao <igor.ormonde@gmail.com>
2023-02-01 04:21:18 -0300

    nvim(chore): remove diagnostics virtual text.

--- config/nvim/lua/plugins/p_lsp/init.lua
+++ config/nvim/lua/plugins/p_lsp/init.lua
@@ -5,6 +5,7 @@ vim.diagnostic.config({
   update_in_insert = true,
   underline = true,
   severity_sort = true,
+  virtual_text = false,
   float = {
     focusable = false,
     style = "minimal",
