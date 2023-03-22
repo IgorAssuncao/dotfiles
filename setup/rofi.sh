27fbc7b3e5c0ee188de849794891cb366897eada
Igor Assuncao <igor.ormonde@gmail.com>
2023-03-21 23:18:35 -0300

    (rofi): add script in setup, missing config.

--- setup/rofi.sh
+++ setup/rofi.sh
@@ -1,23 +1,27 @@
+#!/bin/bash
 # source $HOME/system-config/setup/utils.sh
 
 rofi_exists() {
-  return [[ which rofi ]]
+  $(which rofi)
+  which_rofi_status=$?
+  echo $which_rofi_status
+  return $which_rofi_status
 }
 
 install_rofi() {
-  if [[ rofi_exists ]]; then
+  if [[ rofi_exists -eq 0 ]]; then
     echo """Rofi already exists.
-Nothing to do.
-    """
+Nothing to do."""
+    return
   fi
 
-  # distro=$(cat /etc/os-release | grep -e "^ID=" | awk -F '=' '{ print $NF }')
-  # if [[ $distro == "ubuntu" ]]; then
-  #   apt install -y rofi
-  # fi
-  # if [[ $distro == "manjaro" || $distro == "arch" ]]; then
-  #   pacman -S rofi
-  # fi
+  distro=$(cat /etc/os-release | grep -e "^ID=" | awk -F '=' '{ print $NF }')
+  if [[ $distro == "ubuntu" ]]; then
+    sudo apt install -y rofi
+  fi
+  if [[ $distro == "manjaro" || $distro == "arch" ]]; then
+    sudo pacman -S rofi
+  fi
 }
 
 install_rofi
