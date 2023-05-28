9fdfb25d7f570df7079b8d946ccf216fe23dcd6d
Igor Assuncao <igor.ormonde@gmail.com>
2023-05-28 20:57:10 -0300

    (nvim): change colorscheme.

--- config/nvim/lua/after/init.lua
+++ config/nvim/lua/after/init.lua
@@ -1,6 +1,6 @@
 -- pcall(vim.cmd, "colorscheme habamax")
 -- pcall(vim.cmd, "colorscheme slate")
 
-pcall(vim.cmd, "colorscheme duskfox")
+pcall(vim.cmd, "colorscheme carbonfox")
 
 require("after.colors")
