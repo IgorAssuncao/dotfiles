5fc253bde6d89b252790109189ff00dbe995d974
Igor Assuncao <igor.ormonde@gmail.com>
2023-05-29 00:15:29 -0300

    add void setup, nitrogen and tweak polybar

---
+++ setup/void.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+base_packages=(
+  base-devel
+  curl
+  dbus-elogind
+  dbus-elogind-libs
+  dbus-elignd-x11
+  elogind
+  git
+  make
+  lightdm
+  lightdm-gtk3-greeter
+  seatd
+  zsh
+)
+
+sudo xbps-install -y ${base_packages[@]}
+
+xorg_packages=(
+  picom
+  xorg
+  xinit
+  xrandr
+  leftwm
+  polybar
+)
+
+wayland_packages=(
+  wayland
+  wayfire
+)
+
+sudo xbps-install -y ${xorg_packages[@]}
+
+extra_packages=(
+  alacritty
+  neovim
+  nitrogen
+)
+
+sudo xbps-install -y ${extra_packages[@]}
