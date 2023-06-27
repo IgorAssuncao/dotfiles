02ff735f6ac1dfe51c9081f298e9e698d6c58881
Igor Assuncao <igor.ormonde@gmail.com>
2023-06-27 03:01:40 -0300

    add monitor, width and offset in leftwm and polybar.

--- config/polybar/launch-polybar.sh
+++ config/polybar/launch-polybar.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 for monitor in $(polybar -M | cut -d ':' -f 1); do
-  MONITOR=$monitor polybar -r top &
-  MONITOR=$monitor polybar -r bottom &
+  # leftwm-state -q -n -s "{% for workspace in workspaces %} {{workspace.w}} {{workspace.x}} {% endfor %}" | sed -r '/^\s*$/d' | read -r width x
+  width=$(leftwm-state -q -n -s "{% for workspace in workspaces %} {{workspace.w}} {% endfor %}" | sed -r '/^\s*$/d')
+  x=$(leftwm-state -q -n -s "{% for workspace in workspaces %} {{workspace.x}} {% endfor %}" | sed -r '/^\s*$/d')
+  # width=$(expr $width - 200)
+  width=$(($width - 0))
+  x=$(($x + 0))
+  MONITOR=$monitor width=$width xoffset=$x polybar -r top &
+  MONITOR=$monitor width=$width xoffset=$x polybar -r bottom &
 done
 
