291f8e10b12d40d73fd6b27dc9bc370219731114
Igor Assuncao <igor.ormonde@gmail.com>
2023-08-23 22:33:37 -0300

    (nvim): Noice show cmd_palette and popupmenu together.

--- config/nvim/lua/plugins/p_noice/init.lua
+++ config/nvim/lua/plugins/p_noice/init.lua
@@ -1,22 +1,52 @@
 local status_noice, noice = pcall(require, "noice")
 if not status_noice then
-  vim.notify("Error from plugins.p_noice: noice not found.")
-  return
+    vim.notify("Error from plugins.p_noice: noice not found.")
+    return
 end
 
 noice.setup({
-  lsp = {
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true
+    lsp = {
+        override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true
+        },
     },
-  },
-  presets = {
-    bottom_search = true,
-    command_palette = true,
-    long_message_to_split = true,
-    inc_rename = false,
-    lsp_doc_border = true
-  }
+    presets = {
+        bottom_search = true,
+        command_palette = false,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = true
+    },
+    views = {
+        cmdline_popup = {
+            position = {
+                row = "40%",
+                col = "50%"
+            },
+            size = {
+                width = 60,
+                height = "auto"
+            }
+        },
+        popupmenu = {
+            relative = "editor",
+            position = {
+                row = "50%",
+                col = "50%",
+            },
+            size = {
+                width = 60,
+                height = 10,
+            },
+            border = {
+                style = "rounded",
+                padding = { 0, 1 },
+            },
+            win_options = {
+                winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+            },
+        }
+    }
 })
