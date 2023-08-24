790b9579e0b56947d8a3a2807ab84e44b132955f
Igor Assuncao <igor.ormonde@gmail.com>
2023-08-23 22:47:28 -0300

    (nvim): Notify increase fps and decrease timeout.

--- config/nvim/lua/plugins/p_notify/init.lua
+++ config/nvim/lua/plugins/p_notify/init.lua
@@ -1,13 +1,13 @@
 local status_notify, notify = pcall(require, "notify")
 if not status_notify then
-  vim.notify("Error from plugins.p_notify: notify not found.")
-  return
+    vim.notify("Error from plugins.p_notify: notify not found.")
+    return
 end
 
 notify.setup({
-  background = "#000000",
-  background_color = "#000000",
-  background_colour = "#000000",
-  render = "default",
-  stages = "fade_in_slide_out"
+    background_colour = "#000000",
+    fps = 60,
+    render = "default",
+    stages = "fade_in_slide_out",
+    timeout = 1000
 })
