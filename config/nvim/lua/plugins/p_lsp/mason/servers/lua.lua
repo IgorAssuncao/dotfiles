bee5fa78f9c2cd66a23acc5c0d6aa84164f1926a
Igor Assuncao <igor.ormonde@gmail.com>
2023-10-13 03:26:36 +0000

    (nvim): improve lua workspace.

--- config/nvim/lua/plugins/p_lsp/mason/servers/lua.lua
+++ config/nvim/lua/plugins/p_lsp/mason/servers/lua.lua
@@ -14,11 +14,11 @@ return function(opts)
             },
             workspace = {
                 library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
                     [vim.fn.stdpath("config") .. "/lua"] = true,
-                }
-                -- library = vim.api.nvim_get_runtime_file("", true), -- This adds the path to every plugin that is installed. If this is not the wanted behavior then change library to vim.fn.stdpath("config")
-                -- checkThirdParty = false
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    vim.api.nvim_get_runtime_file("", true)
+                },
+                checkThirdParty = false
             }
         }
     }
