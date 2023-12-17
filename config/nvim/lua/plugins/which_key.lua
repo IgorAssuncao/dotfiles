1f3b077a33b8af5da41fda285985c2364309c402
Igor Assuncao <igor.ormonde@gmail.com>
2023-12-17 02:56:22 +0000

    (nvim): migrate which-key to lazy config.

--- config/nvim/lua/plugins/which_key.lua
+++ config/nvim/lua/plugins/which_key.lua
@@ -1,18 +1,23 @@
-local status_which_key, which_key = pcall(require, "which-key")
-if not status_which_key then
-    vim.notify("Error from plugins.p_undotree.keymaps: which-key not found.")
-    return
-end
-
-which_key.register({
-    ["<leader>W"] = {
+return {
+  "folke/which-key.nvim",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 200
+    require("which-key").setup {}
+  end,
+  config = function()
+    local which_key = require("which-key")
+    which_key.register({
+      ["<leader>W"] = {
         W = { function() vim.cmd { cmd = "WhichKey" } end, "[W]hich [K]ey Toggle" },
         N = { "<cmd>WhichKey '' n<cr>", "[W]hich [K]ey [N]ormal mode Toggle" },
         I = { "<cmd>WhichKey '' i<cr>", "[W]hich [K]ey [I]nsert mode Toggle" },
         V = { "<cmd>WhichKey '' v<cr>", "[W]hich [K]ey [V]isual mode Toggle" },
         C = { "<cmd>WhichKey '' c<cr>", "[W]hich [K]ey [C]ommand mode Toggle" },
-    },
-    ["<leader>L"] = {
+      },
+      ["<leader>L"] = {
         name = "[L]anguages tools"
-    },
-})
+      }
+    })
+  end
+}
