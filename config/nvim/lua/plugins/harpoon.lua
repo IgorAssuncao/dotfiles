93310dc733272950185815ec8216eb54b4fff451
Igor Assuncao <igor.ormonde@gmail.com>
2023-12-17 13:35:10 +0000

    (nvim): migrate harpoon to lazy config.

---
+++ config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,20 @@
+return {
+  "theprimeagen/harpoon",
+  -- TODO: Load this plugin on keymap.
+  -- lazy = true,
+  -- keys = {
+  --   { "<leader>h", "", desc = "[H]arpoon" }
+  -- },
+  config = function()
+    local mark = require('harpoon.mark')
+    local ui = require('harpoon.ui')
+
+    require("which-key").register({
+      h = {
+        name = "[H]arpoon",
+        a = { mark.add_file, "Mark [a]dd file" },
+        m = { ui.toggle_quick_menu, "UI Toggle" }
+      }
+    }, { prefix = "<leader>" })
+  end
+}
