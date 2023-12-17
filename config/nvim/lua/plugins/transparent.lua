a3fb44c4a2064430a9a15f8fc64e4e22e725c87d
Igor Assuncao <igor.ormonde@gmail.com>
2023-12-17 03:30:55 +0000

    (nvim): migrate transparent to lazy config.

---
+++ config/nvim/lua/plugins/transparent.lua
@@ -0,0 +1,25 @@
+vim.opt.pumblend = 30
+vim.opt.winblend = 30
+-- vim.opt.hightlight-blend = 30
+
+-- vim.g.transparent_groups = vim.list_extend(
+--   vim.g.transparent_groups or {},
+--   vim.tbl_map(function(value)
+--     return value.hl_group
+--   end, vim.tbl_values(require("bufferline.config").highlights or {}))
+-- )
+
+return {
+  "xiyaowong/transparent.nvim",
+  config = function()
+    require("transparent").setup({
+      extra_groups = {
+        "NormalFloat",
+        "NvimTreeNormal",
+        "NotifyBackground"
+      }
+    })
+  end
+
+
+}
