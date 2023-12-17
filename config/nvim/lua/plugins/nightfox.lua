a2ddd75cd166286ba30266d34d2401db1512ab77
Igor Assuncao <igor.ormonde@gmail.com>
2023-12-17 03:39:22 +0000

    (nvim): add nightfox opts.

--- config/nvim/lua/plugins/nightfox.lua
+++ config/nvim/lua/plugins/nightfox.lua
@@ -1,6 +1,12 @@
 return {
   "EdenEast/nightfox.nvim",
   name = "nightfox",
+  opts = {
+    options = {
+      dim_inactive = true,
+      transparent = true
+    }
+  }
   -- config = function()
   --   vim.cmd("colorscheme duskfox")
   --   vim.cmd("colorscheme carbonfox")
