4468a8742cd5e6049e9d6c795c3de1ceea7a19eb
Igor Assuncao <igor.ormonde@gmail.com>
2023-12-17 13:35:48 +0000

    (nvim): migrate leap to lazy config.

--- config/nvim/lua/plugins/p_leap/init.lua
+++
@@ -1,7 +0,0 @@
-local status_leap, leap = pcall(require, "leap")
-if not status_leap then
-    vim.notify("Error from plugins.p_leap: leap not found.")
-    return
-end
-
-leap.add_default_mappings()
