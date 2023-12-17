92f1ac0b4fb1b194a50ae0daac4f0b8b69c5318a
Igor Assuncao <igor.ormonde@gmail.com>
2023-12-17 02:55:16 +0000

    (nvim): Install lazy and remove packer references.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,9 @@
+vim.g.mapleader = " "
+
+-- Bootstrap package manager first
+-- because I use whick key as the default keymapper
+require("lazy_setup")
+
 require("base")
 require("plugins")
 require("after")
