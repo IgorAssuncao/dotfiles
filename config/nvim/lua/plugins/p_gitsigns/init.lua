7f4f277a50c2fcfaf3de8af8e81214122e1f640c
Igor Assuncao <igor.ormonde@gmail.com>
2023-12-17 13:34:46 +0000

    (nvim): migrate gitsigns to lazy config.

--- config/nvim/lua/plugins/p_gitsigns/init.lua
+++
@@ -1,7 +0,0 @@
-local status_gitsigns, gitsigns = pcall(require, "gitsigns")
-if not status_gitsigns then
-    vim.notify("Error from plugins.p_gitsigns: gitsigns not found.")
-    return
-end
-
-gitsigns.setup()
