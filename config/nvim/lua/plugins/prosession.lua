19e59e0af1558503b22e49c26d6d3c4844b00678
Igor Assuncao <igor.ormonde@gmail.com>
2023-12-17 13:36:31 +0000

    (nvim): migrate prosession to lazy config.

---
+++ config/nvim/lua/plugins/prosession.lua
@@ -0,0 +1,10 @@
+return {
+  -- Still has to learn its purpose and how it can be useful to me.
+  "dhruvasagar/vim-prosession",
+  dependencies = { "tpope/vim-obsession", cmd = "Prosession" },
+  config = function()
+    vim.g.prosession_on_startup = 1
+    vim.g.prosession_default_session = 1
+    vim.g.prosession_per_branch = 1
+  end
+}
