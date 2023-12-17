2e6e49427655353e477276e097f51857e9fdbf7f
Igor Assuncao <igor.ormonde@gmail.com>
2023-12-17 13:47:13 +0000

    (nvim): comment out lazy bootstrap.

--- config/nvim/lua/lazy_setup.lua
+++ config/nvim/lua/lazy_setup.lua
@@ -18,32 +18,4 @@ local ensure_lazy = function()
     vim.opt.rtp:prepend(lazypath)
 end
 
---TODO: REMOVE THIS PRINT
-print(vim.opt.rtp)
-
-local lazy_bootstrap = ensure_lazy()
-
--- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- if not vim.loop.fs_stat(lazypath) then
---     vim.fn.system({
---         "git",
---         "clone",
---         "--filter=blob:none",
---         "https://github.com/folke/lazy.nvim.git",
---         "--branch=stable", -- latest stable release
---         lazypath,
---     })
--- end
--- vim.opt.rtp:prepend(lazypath)
-
--- Use a protected call so packer don't error
--- out in first use
-local status_lazy, lazy = pcall(require, "lazy")
-if not status_lazy then
-    vim.notify("Error from plugins.lazy: lazy not found.")
-    return
-end
-
--- if lazy_bootstrap then
--- end
-lazy.setup("plugins")
+ensure_lazy()
