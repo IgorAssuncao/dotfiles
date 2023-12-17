29f12cc7046de8cdd4b5c1156b6f09f3ac6b7450
Igor Assuncao <igor.ormonde@gmail.com>
2023-12-17 13:35:22 +0000

    (nvim): migrate illuminate to lazy config.

---
+++ config/nvim/lua/plugins/illuminate.lua
@@ -0,0 +1,17 @@
+return {
+  "RRethy/vim-illuminate",
+  config = function()
+    local illuminate = require("illuminate")
+    illuminate.configure()
+
+    require("which-key").register({
+      ["<leader>"] = {
+        i = {
+          name = "[I]lluminate",
+          n = { function() illuminate.goto_next_reference() end, "[N]ext reference" },
+          p = { function() illuminate.goto_prev_reference() end, "[P]revious reference" }
+        }
+      }
+    })
+  end
+}
