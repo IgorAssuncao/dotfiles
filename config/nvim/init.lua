428b203aee5e5db57ce3f4b7dde38bcd09306573
Igor Assuncao <igor.ormonde@gmail.com>
2023-12-17 13:45:59 +0000

    (nvim): improve lazy setup and make it load folders inside plugins folder.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,4 @@
+-- Lazy explicitly asks to map the leader key before loading itself.
 vim.g.mapleader = " "
 
 -- Bootstrap package manager first
@@ -5,5 +6,33 @@ vim.g.mapleader = " "
 require("lazy_setup")
 
 require("base")
-require("plugins")
+
+-- Use a protected call so lazy don't error
+-- out in first use
+local status_lazy, lazy = pcall(require, "lazy")
+if not status_lazy then
+    vim.notify("Error from plugins.lazy: lazy not found.")
+    return
+end
+
+lazy.setup({
+    {
+        "iamcco/markdown-preview.nvim",
+        build = "cd app & npm install",
+        config = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" }
+    },
+    spec = {
+        { import = "plugins" },
+        { import = "plugins.lsp" },
+        { import = "plugins.debug_adapter" }
+    }
+})
+
+-- keymaps is being required after lazy
+-- because of which-key plugin that registers keymaps.
+require("base.keymaps")
+
 require("after")
