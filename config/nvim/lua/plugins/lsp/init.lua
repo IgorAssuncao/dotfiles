c230e0895883dab233f0fc41ceea7e8cd6f0ce85
Igor Assuncao <igor.ormonde@gmail.com>
2023-12-17 13:44:17 +0000

    (nvim): migrate lsp setup to lazy config.

---
+++ config/nvim/lua/plugins/lsp/init.lua
@@ -0,0 +1,14 @@
+vim.diagnostic.config({
+  update_in_insert = true,
+  underline = false,
+  severity_sort = true,
+  virtual_text = true,
+  float = {
+    focusable = false,
+    style = "minimal",
+    border = "rounded",
+    source = "always"
+  }
+})
+
+return {}
