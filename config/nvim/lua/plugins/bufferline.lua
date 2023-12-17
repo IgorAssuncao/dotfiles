e1b2644bec1e36a00011bfb3f3511e093f50dc62
Igor Assuncao <igor.ormonde@gmail.com>
2023-12-17 03:30:33 +0000

    (nvim): activate bufferline (i forgot to setup before).

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -3,5 +3,16 @@ return {
   -- version = 'v4.*',
   dependencies = {
     "nvim-tree/nvim-web-devicons"
+  },
+  opts = {
+    options = {
+      active = true,
+      always_show_bufferline = true,
+      numbers = "ordinal",
+      diagnostics_update_in_insert = true
+    }
   }
+  -- config = function()
+  --     require("bufferline").setup()
+  -- end
 }
