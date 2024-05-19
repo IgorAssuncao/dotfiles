b26d52dc6ce3ad4abe92b61b9173feba504e3688
Igor Assuncao <igor.ormonde@gmail.com>
2024-05-19 00:44:30 -0300

    (nvim): clearer text.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,7 +2,7 @@
 vim.g.mapleader = " "
 
 -- Bootstrap package manager first
--- because I use whick key as the default keymapper
+-- Necessary since I use whick key as the default keymapper
 require("lazy_setup")
 
 require("base")
