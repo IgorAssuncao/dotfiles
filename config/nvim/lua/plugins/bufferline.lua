bbcf1ad0bec0388cb491f0db29b2ea4b32af503d
IgorAssuncao <igor@rivalry.com>
2024-05-20 17:16:12 -0300

    (nvim): change identation to 4 spaces.

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -1,19 +1,19 @@
 return {
-  "akinsho/bufferline.nvim",
-  version = '*',
-  dependencies = {
-    "git@github.com:nvim-tree/nvim-web-devicons.git"
-    -- "nvim-tree/nvim-web-devicons"
-  },
-  opts = {
-    options = {
-      active = true,
-      always_show_bufferline = true,
-      numbers = "ordinal",
-      diagnostics_update_in_insert = true
+    "akinsho/bufferline.nvim",
+    version = '*',
+    dependencies = {
+        "git@github.com:nvim-tree/nvim-web-devicons.git"
+        -- "nvim-tree/nvim-web-devicons"
+    },
+    opts = {
+        options = {
+            active = true,
+            always_show_bufferline = true,
+            numbers = "ordinal",
+            diagnostics_update_in_insert = true
+        }
     }
-  }
-  -- config = function()
-  --     require("bufferline").setup()
-  -- end
+    -- config = function()
+    --     require("bufferline").setup()
+    -- end
 }
