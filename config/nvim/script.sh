977842222d15edd6ea07c471b84d04911ac1791c
IgorAssuncao <igor@rivalry.com>
2024-07-03 17:15:59 -0300

    Remove file.

--- config/nvim/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-body="## What's Changed
-  * [PF-832] Notify release on slack. by @IgorAssuncao in https://github.com/lootmarket/rivalry/pull/9012
-  * Adding sentry logging of user data for SEON requests by @ryantology in https://github.com/lootmarket/rivalry/pull/9013
-"
-
-echo "$body" | tr '\n' ';' | sed -e 's/;/\\n/g'
