1c08f5845b9910b9b1bb9b2244afd45be8c64e8b
Igor Assuncao <igor.ormonde@gmail.com>
2025-05-27 11:34:30 -0300

    nvim(chore): remove some plugins.

--- config/nvim/lua/plugins/alpha.lua
+++ config/nvim/lua/plugins/alpha.lua
@@ -1,39 +1,39 @@
 -- Plugin that allows a nicer startup menu.
 
-vim.api.nvim_create_autocmd("VimEnter", {
-    once = true,
-    callback = function()
-        math.randomseed(os.time())
-        local fg_color = tostring(math.random(0, 12))
-        local hi_setter = "hi AlphaHeader ctermfg="
-        vim.cmd(hi_setter .. fg_color)
-    end
-})
-
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--     once = true,
+--     callback = function()
+--         math.randomseed(os.time())
+--         local fg_color = tostring(math.random(0, 12))
+--         local hi_setter = "hi AlphaHeader ctermfg="
+--         vim.cmd(hi_setter .. fg_color)
+--     end
+-- })
+--
 return {
-    "goolord/alpha-nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons"
-    },
-    config = function()
-        local dashboard = require("alpha.themes.dashboard")
-        dashboard.section.header.val = {
-            " ██╗    ██████╗      ██████╗     ███╗   ██╗ ",
-            " ██║    ██╔══██╗    ██╔═████╗    ████╗  ██║ ",
-            " ██║    ██████╔╝    ██║██╔██║    ██╔██╗ ██║ ",
-            " ██║    ██╔══██╗    ████╔╝██║    ██║╚██╗██║ ",
-            " ██║    ██║  ██║    ╚██████╔╝    ██║ ╚████║ ",
-            " ╚═╝    ╚═╝  ╚═╝     ╚═════╝     ╚═╝  ╚═══╝ ",
-        }
-        dashboard.section.buttons.val = vim.tbl_deep_extend(
-            "force",
-            dashboard.section.buttons.val,
-            {
-                dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
-                dashboard.button("SPC p", "Open Recent Projects", ":Telescope projects<CR>")
-            }
-        )
-        require("alpha").setup(dashboard.config)
-        -- require("alpha").setup(require("alpha.themes.startify").config)
-    end
+    --     "goolord/alpha-nvim",
+    --     dependencies = {
+    --         "nvim-tree/nvim-web-devicons"
+    --     },
+    --     config = function()
+    --         local dashboard = require("alpha.themes.dashboard")
+    --         dashboard.section.header.val = {
+    --             " ██╗    ██████╗      ██████╗     ███╗   ██╗ ",
+    --             " ██║    ██╔══██╗    ██╔═████╗    ████╗  ██║ ",
+    --             " ██║    ██████╔╝    ██║██╔██║    ██╔██╗ ██║ ",
+    --             " ██║    ██╔══██╗    ████╔╝██║    ██║╚██╗██║ ",
+    --             " ██║    ██║  ██║    ╚██████╔╝    ██║ ╚████║ ",
+    --             " ╚═╝    ╚═╝  ╚═╝     ╚═════╝     ╚═╝  ╚═══╝ ",
+    --         }
+    --         dashboard.section.buttons.val = vim.tbl_deep_extend(
+    --             "force",
+    --             dashboard.section.buttons.val,
+    --             {
+    --                 dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+    --                 dashboard.button("SPC p", "Open Recent Projects", ":Telescope projects<CR>")
+    --             }
+    --         )
+    --         require("alpha").setup(dashboard.config)
+    --         -- require("alpha").setup(require("alpha.themes.startify").config)
+    --     end
 }
--- config/nvim/lua/plugins/nightfox.lua
+++ config/nvim/lua/plugins/nightfox.lua
@@ -1,14 +1,14 @@
 return {
-  "EdenEast/nightfox.nvim",
-  name = "nightfox",
-  opts = {
-    options = {
-      dim_inactive = true,
-      transparent = true
-    }
-  }
-  -- config = function()
-  --   vim.cmd("colorscheme duskfox")
-  --   vim.cmd("colorscheme carbonfox")
-  -- end
+    -- "EdenEast/nightfox.nvim",
+    -- name = "nightfox",
+    -- opts = {
+    --   options = {
+    --     dim_inactive = true,
+    --     transparent = true
+    --   }
+    -- }
+    -- config = function()
+    --   vim.cmd("colorscheme duskfox")
+    --   vim.cmd("colorscheme carbonfox")
+    -- end
 }
