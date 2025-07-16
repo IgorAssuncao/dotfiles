08b4ecb65eb80f09cc476e7737f7c30b97829768
Igor Assuncao <igor.ormonde@gmail.com>
2025-07-15 23:43:02 -0300

    nvim(feat): indent_blankline enable scope highlighting.

--- config/nvim/lua/plugins/indent_blankline.lua
+++ config/nvim/lua/plugins/indent_blankline.lua
@@ -5,33 +5,10 @@ vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "eol:↴"
 -- vim.opt.listchars:append "tab:>"
 
-local background_highlight_groups = {
-    "CursorColumn",
-    "Whitespace"
-}
-
 return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    opts = {
-        indent = {
-            highlight = background_highlight_groups,
-            smart_indent_cap = true
-        },
-        scope = {
-            enabled = true,
-            highlight = BASE.defaults.highlight_groups,
-            show_start = true,
-            show_end = true
-        },
-        whitespace = {
-            highlight = background_highlight_groups,
-            remove_blankline_trail = true
-        },
-        -- show_end_of_line = true,
-        -- space_char_blankline = " "
-    },
-    config = function(_, opts)
+    config = function()
         local indent_blankline_hooks = require("ibl.hooks")
 
         indent_blankline_hooks.register(
@@ -44,25 +21,21 @@ return {
             indent_blankline_hooks.builtin.scope_highlight_from_extmark
         )
 
-        require("ibl").setup()
-        -- require("ibl").setup {
-        --     indent = {
-        --         highlight = background_highlight_groups,
-        --         smart_indent_cap = true
-        --     },
-        --     scope = {
-        --         enabled = true,
-        --         highlight = BASE.defaults.highlight_groups,
-        --         show_start = true,
-        --         show_end = true,
-        --         show_exact_scope = true
-        --     },
-        --     whitespace = {
-        --         highlight = background_highlight_groups,
-        --         remove_blankline_trail = true
-        --     },
-        --     -- show_end_of_line = true,
-        --     -- space_char_blankline = " "
-        -- }
+        vim.g.rainbow_delimiters = { highlight = BASE.defaults.highlight_groups }
+        require("ibl").setup {
+            -- indent = {
+            --     highlight = BASE.defaults.highlight_groups,
+            --     smart_indent_cap = true,
+            --     char = "|"
+            -- },
+            scope = {
+                enabled = true,
+                highlight = BASE.defaults.highlight_groups,
+                show_start = true,
+                show_end = true,
+                show_exact_scope = true,
+                char = "|"
+            }
+        }
     end
 }
