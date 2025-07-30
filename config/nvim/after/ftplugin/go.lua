2766397bec2696dddaeb8b4556107b104b9cb9a5
Igor Assuncao <igor.ormonde@gmail.com>
2025-07-30 13:11:06 -0300

    nvim(chore): make GoImpl clearer, add GoTag mapstructure.

--- config/nvim/after/ftplugin/go.lua
+++ config/nvim/after/ftplugin/go.lua
@@ -2,13 +2,15 @@ local bufnr = vim.api.nvim_get_current_buf()
 
 require("which-key").add({
     { "<leader>Lg",    group = "[G]olang Gopher" },
-    { "<leader>Lge",   function() vim.cmd { cmd = "GoIfErr" } end,                                                                          desc = "If [E]rr" },
-    { "<leader>Lgi",   function() vim.cmd { cmd = "GoImpl", args = { vim.fn.input("<receiver_name> <receiver_type> <interface>: ") } } end, desc = "[I]mplement Interface" },
+    { "<leader>Lge",   function() vim.cmd { cmd = "GoIfErr" } end,                                                                  desc = "If [E]rr" },
+    { "<leader>Lgi",   function() vim.cmd { cmd = "GoImpl", args = { vim.fn.input("(Struct under cursor) Interface name:") } } end, desc = "[I]mplement Interface" },
     { "<leader>Lgt",   group = "Manage [T]ags" },
     { "<leader>Lgta",  group = "Add [T]ags" },
-    { "<leader>Lgtaj", function() vim.cmd { cmd = "GoTagAdd", args = { "json" } } end,                                                      desc = "[J]son tags" },
-    { "<leader>Lgtay", function() vim.cmd { cmd = "GoTagAdd", args = { "yaml" } } end,                                                      desc = "[Y]aml tags" },
+    { "<leader>Lgtaj", function() vim.cmd { cmd = "GoTagAdd", args = { "json" } } end,                                              desc = "[J]son tags" },
+    { "<leader>Lgtay", function() vim.cmd { cmd = "GoTagAdd", args = { "yaml" } } end,                                              desc = "[Y]aml tags" },
+    { "<leader>Lgtam", function() vim.cmd { cmd = "GoTagAdd", args = { "mapstructure" } } end,                                      desc = "[Y]aml tags" },
     { "<leader>Lgtr",  group = "Remove [T]ags" },
-    { "<leader>Lgtrj", function() vim.cmd { cmd = "GoTagRm", args = { "json" } } end,                                                       desc = "[J]son tags" },
-    { "<leader>Lgtry", function() vim.cmd { cmd = "GoTagRm", args = { "yaml" } } end,                                                       desc = "[Y]aml tags" },
+    { "<leader>Lgtrj", function() vim.cmd { cmd = "GoTagRm", args = { "json" } } end,                                               desc = "[J]son tags" },
+    { "<leader>Lgtry", function() vim.cmd { cmd = "GoTagRm", args = { "yaml" } } end,                                               desc = "[Y]aml tags" },
+    { "<leader>Lgtrm", function() vim.cmd { cmd = "GoTagAdd", args = { "mapstructure" } } end,                                      desc = "[Y]aml tags" },
 }, { buffer = bufnr })
