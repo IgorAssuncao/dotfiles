1c8df4d50c0aa8eb87505a99eca32e6019bdce82
Igor Assuncao <igor.ormonde@gmail.com>
2025-08-01 04:16:43 -0300

    nvim(feat): add schemastore for json and yaml schemas.

--- config/nvim/lua/plugins/lsp/servers/yaml.lua
+++ config/nvim/lua/plugins/lsp/servers/yaml.lua
@@ -1,10 +1,17 @@
 return {
     settings = {
         yaml = {
-            schemas = {
-                ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/**.yml",
-                ["https://json.schemastore.org/github-action.json"] = "/.github/actions/**.yml",
-            }
+            schemaStore = {
+                -- NOTE: Disable to use schemastore plugin.
+                enable = false,
+                -- NOTE: schemastore plugin
+                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                url = ""
+            },
+            schemas = vim.tbl_deep_extend("force", {
+                ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/**.(yml|yaml)",
+                ["https://json.schemastore.org/github-action.json"] = "/.github/actions/**.(yml|yaml)",
+            }, require('schemastore').yaml.schemas())
         }
     },
     filetypes = {
