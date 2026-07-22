5b4b67731039c198fa8945e02f4afba729c6ef28
NSX001738 <nsx001738@nsx001738.tail2b41c.ts.net>
2026-07-22 09:22:38 -0300

    rustup(feat): install tools.

--- setup/rustup.sh
+++ setup/rustup.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+
+# Install tools
+# TODO: Create script for reloading bash session and install tools.
+# tools=(bat eza ripgrep)
+cargo search ripgrep
+cargo search eza
+cargo search bat
