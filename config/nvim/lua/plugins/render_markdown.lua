9a15fb40f28eec4659ca83ad397fac890c25b28c
IgorAssuncao <igor.assuncao@nsx.bet>
2026-09-16 12:18:44 -0300

    nvim(refac): rename render_markdown plugin file.

--- config/nvim/lua/plugins/render_markdown.lua
+++ config/nvim/lua/plugins/render_markdown.lua
@@ -3,6 +3,7 @@ return {
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
     ---@module 'render-markdown'
     -- ---@type render.md.UserConfig
+    ft = { "markdown" },
     opts = {
         completions = {
             lsp = {
