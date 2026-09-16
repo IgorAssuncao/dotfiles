3e89187d9eb870fdaf35de9933af972a262b0e97
Igor Assuncao <igor.ormonde@gmail.com>
2026-09-15 22:02:35 -0300

    setup(rustup): fix curl command, add fd tool.

--- setup/rustup.sh
+++ setup/rustup.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh # -s -- -y
 
 
 # Install tools
 # TODO: Create script for reloading bash session and install tools.
-# tools=(bat eza ripgrep)
-cargo search ripgrep
-cargo search eza
-cargo search bat
+# tools=(bat eza ripgrep fd)
+# cargo search ripgrep
+# cargo search eza
+# cargo search bat
+# cargo search fd
